5d6fba6e8305efdd4782065f4652e42b9a652194
Arnau Oncins Rodríguez <arnau.oncins@gmail.com>
2020-10-30 07:06:19 +0100

    Removed unused MessageParserService.

--- src/main/java/info/nauar/irc/pickupBot/message/parser/MessageParserService.java
+++
@@ -1,6 +0,0 @@
-package info.nauar.irc.pickupBot.message.parser;
-
-public interface MessageParserService {
-
-    void parse(String message);
-}
--- src/main/java/info/nauar/irc/pickupBot/message/parser/MessageParserServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package info.nauar.irc.pickupBot.message.parser;
-
-public class MessageParserServiceImpl implements MessageParserService {
-    @Override
-    public void parse(String message) {
-
-    }
-}
